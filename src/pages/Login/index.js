935bdee24a7641601d038276d5d7f48798f83962
Guilherme Ferrari <guiferrari10@hotmail.com>
2019-08-10 15:38:23 -0300

    Added page of Devs with stylization

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../../services/api';
 
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
 
-export default function Login() {
+export default function Login({ history }) {
+
+  const [value, setValue] = useState('');
+
+  async function handleSubmite(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      username: value,
+    })
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`)
+  }
+
   return (
     <Container>
-      <form>
+      <form onSubmit={handleSubmite}>
         <img src={Logo} alt="Logo tindev" />
-        <input placeholder="Digite seu usuário do Github" />
+        <input
+          placeholder="Digite seu usuário do Github"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
         <button type="submit">Enviar</button>
       </form>
     </Container>
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,87 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container } from './styles';
 
-// import { Container } from './styles';
+import Logo from '../../assets/logo.svg';
+import Like from '../../assets/like.svg';
+import Deslike from '../../assets/dislike.svg';
 
-export default function Main() {
+export default function Main({ match }) {
+
+  useEffect(() => {
+    async function loadUser() {
+
+    }
+
+    loadUser();
+  }, [match.params.id]);
+
   return (
-    <h1>Olá mundo!</h1>
+    <Container>
+      <div className="logo">
+        <img src={Logo} alt="Logo tindev" />
+      </div>
+      <ul>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>Programador</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={Like} alt="Deslike" />
+            </button>
+            <button type="button">
+              <img src={Deslike} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>Programador</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={Like} alt="Deslike" />
+            </button>
+            <button type="button">
+              <img src={Deslike} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>Programador</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={Like} alt="Deslike" />
+            </button>
+            <button type="button">
+              <img src={Deslike} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
+          <footer>
+            <strong>Diego Fernandes</strong>
+            <p>Programador</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={Like} alt="Deslike" />
+            </button>
+            <button type="button">
+              <img src={Deslike} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </Container>
   );
 }
